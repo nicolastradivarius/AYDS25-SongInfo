553502566b020218917b47c3a033056b85a80d60
nicolastradivarius <gonzaleznicolas114@outlook.com>
2025-05-02 18:36:01 -0300

    Propiedad "releaseDatePrecision" agregada

--- app/src/main/java/ayds/songinfo/home/model/entities/SpotifySong.kt
+++ app/src/main/java/ayds/songinfo/home/model/entities/SpotifySong.kt
@@ -7,6 +7,7 @@ sealed class Song {
         val artistName: String,
         val albumName: String,
         val releaseDate: String,
+        val releaseDatePrecision: String,
         val spotifyUrl: String,
         val imageUrl: String,
         var isLocallyStored: Boolean = false
